c5e198d0908eac006fd76cce1045dd66fea331cc
Richard Redder Pedersen <richard7770@gmail.com>
2016-09-02 14:43:08 +0200

    Use util and batch insert.

--- process.js
+++ process.js
@@ -1,27 +1,16 @@
 
 //  Group names to create
-var primaryName = '3Spejdere';
+var primaryName = 'Spejdere';
 var secondaryName = 'Forældre';
 
-
+var auth = {};
 $(function(){
-  getToken()              //  Resolves empty
-  .then(verifyToken)      //  Resolves as ajax
+  getToken() &&
+  verifyToken()           //  Resolves as ajax
   .then(establishChannel) //  Resolves with csv
-  .then(handleCsv)        //  Pass through
+  .then(handleCsv)
 });
 
-//  Parse URL fragment
-var auth = {};
-function getToken() {
-  var m, re=/(\w+)=([^&]*)/g;
-  var dfd = $.Deferred();
-  while( m = re.exec(window.location.hash))
-    auth[m[1]] = decodeURIComponent(m[2]);
-  if (auth.access_token) dfd.resolve()
-  else dfd.reject()
-  return dfd;
-}
 
 var client_id = '339808440187-1l9an0suebhb8rkidhd6bseemc07lck9.apps.googleusercontent.com';
 var verifyUrl = 'https://www.googleapis.com/oauth2/v3/tokeninfo';
@@ -96,7 +85,7 @@ function handleCsv(csv) {
       value = record[rawEmail[r]];
       if( value) gcEntry.gd$email.push({
         'rel':rel[r],
-        '$t':value,
+        'address':value,
         'primary': r=='home'
       });
     }
@@ -124,7 +113,7 @@ function handleCsv(csv) {
   for(record of records) if( !record.isPrimary )
     secondary.push(record);
   createAndFillGroup( primaryName, primary);
-  // createAndFillGroup( secondaryName, secondary);
+  createAndFillGroup( secondaryName, secondary);
 }
 
 function parseCsv( csv, recordCallback) {
@@ -164,40 +153,18 @@ function parseCsv( csv, recordCallback) {
 function createAndFillGroup( groupName, contacts) {
   console.log('createAndFillGroup', arguments);
   createGroup(groupName).then((data,status,xhr)=>{
-    var record, groupid = data.entry.id.$t;
+    var groupid = data.entry.id.$t;
+    var updatedContacts=[], newContacts=[];
     for (record of contacts) {
       record.gcEntry.gContact$groupMembershipInfo.push({'href':groupid });
-      createContact(record.gcEntry);
-      break;
+      // if(record.gcEntry.id)
+      //   updatedContacts.push(record.gcEntry)
+      // else
+      newContacts.push(record.gcEntry)
     }
+    // if(updatedContacts.length)
+    //   updateContact(updatedContacts);
+    // if(newContacts.length)
+    createContacts(newContacts);
   });
-}
-
-function createGroup(groupName) {
-  if(!groupName)  groupName = 'G'+Date.now();
-  console.log('createGroup', groupName);
-  return $.ajax({
-    'method':'post',
-    'url':'https://content.googleapis.com/m8/feeds/groups/default/full',
-    'contentType':'application/json',
-    'headers':{
-      'gdata-version':'3.0',
-      'authorization':'Bearer '+auth.access_token,
-      },
-    'data': JSON.stringify( {'entry':{'title':{'$t':groupName}}})
-  }).always(console.log);
-}
-
-function createContact( gcEntry) {
-  console.log('createContact', gcEntry.gd$name.gd$fullName.$t, gcEntry);
-  return $.ajax({
-    'method':'post',
-    'url':'https://content.googleapis.com/m8/feeds/contacts/default/full',
-    'contentType':'application/json',
-    'headers':{
-      'gdata-version':'3.0',
-      'authorization':'Bearer '+auth.access_token,
-      },
-    'data': JSON.stringify( {'entry':gcEntry})
-  }).always(console.log);
 }
