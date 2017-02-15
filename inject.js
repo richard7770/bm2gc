485e6032f242a3e0808ddba5dd166bc3bf86f7af
Richard Redder Pedersen <richard7770@gmail.com>
2017-02-15 16:12:12 +0100

    pit

--- inject.js
+++ inject.js
@@ -1,54 +1,50 @@
-//  javascript: (function(){document.head.appendChild(document.createElement('script')).src = 'https://rawgit.com/richard7770/bm2gc/master/load.js'})()
+//  javascript: (function(){document.head.appendChild(document.createElement('script')).src = 'https://rawgit.com/richard7770/dds2contacts/master/load.js'})()
 //  javascript: (function(){document.head.appendChild(document.createElement('script')).src = 'https://localhost:3541/inject.js'})()
 (function() {
   var exportFields = "member_number name mobile active_function_ids/id";
   //  Fragment for main.html
-  var mainFragment = {
+  var fragmentState = {
     secret: 'g'+Math.floor(1e9*Math.random()),
-    debug:  'local',
+    debug: true
   };
   var currentScript = $('head>script:last-child')[0];
   //  Parameters for Google authorization
-  var auth = {
+  var googleOauthParam = {
     'client_id': '637601984471-e5tn9qk8mds51b069vgd7tap5fl801re.apps.googleusercontent.com',
     'scope': 'https://www.google.com/m8/feeds/',
     'response_type': 'token',
-    'state': JSON.stringify(mainFragment),
+    'state': JSON.stringify(fragmentState),
     'redirect_uri': currentScript.src.replace( /[^/]+$/, 'main.html'),
   };
-  //  URL for Google authorization
+  //  Generate URL for Google authorization
   var authurl = 'https://accounts.google.com/o/oauth2/v2/auth?' +
-    Object.keys(auth).map(
-      key => key+'='+encodeURIComponent(auth[key])
+    Object.keys(googleOauthParam).map(
+      key => key+'='+encodeURIComponent(googleOauthParam[key])
     ).join('&');
 
   //  Entrypoint
   if( window.location.href.startsWith('https://medlem.dds.dk/web') ) {
     $.when( 
-        fetchCsv(),         //  Fetch the list of members from DDS,
-        openMainWindow()    //  while starting the authorization process with google.
-    ).then( function(xhr,port){   //  Then post the list as a message to main.html.
-      port.postMessage(xhr[0]);
+        fetchData(),                              //  Fetch the list of functions and members from DDS,
+        openMainWindow()                          //  while starting the authorization process with google.
+    ).then(                 
+      function( data, port){   
+        port.postMessage( JSON.stringify(data));  //  Then post the list as a message to main.html.
     });
   }
   else
-    alert('Dette script kan kun bruges på fanebladet "Medlemmer" i DDS Medlemsservice');
+    alert('Dette script kan kun bruges i DDS Medlemsservice');
 
-  //  Returns an ajax promise
-  function fetchCsv() {
-    var data = {
-      "model":"member.profile",
-      "fields": exportFields.split(' ')
-        .map( function(f){
-          var [n,l] = f.split(':');
-          return {name: n, label: l||f};
-        }),
-      "ids":false,
-      "domain":[["state","=","active"]],
-      "import_compat":true};
-    return $.post('/web/export/csv', {
-      data: JSON.stringify( data),
-      token: Date.now() });
+  //  Returns promise of object structure
+  function fetchData() {
+    var fcn = new openerp.Model( 'member.function');
+    var mbr = new openerp.Model( 'member.member');
+    return $.when(
+        fcn.query( 'id member_id organization_id function_type_id'.split(' ')).all(),
+        mbr.query( 'id name member_number email mobile phone complete_address birthdate comment school school_class_number school_class_letter'
+          .split(' ')).all()
+        )
+      .then( (f,m)=>({fcn:f, mbr:m}));
   }
 
   //  Returns a port promise
@@ -56,7 +52,7 @@
     var dfr = $.Deferred();
     window.addEventListener('message', function listener(ev) {
       if(     ev.source === processWindow 
-          &&  ev.data == mainFragment.secret 
+          &&  ev.data == fragmentState.secret 
           &&  ev.ports.length == 1) {
         window.removeEventListener('message', listener);
         dfr.resolve( ev.ports.shift());
--- main.js
+++ main.js
@@ -1,99 +1,50 @@
 
 $(function(){
   parseFragment();
-  var contacts,csv;
+  var contacts, members;
   if( fragment.debug == 'local') {
-    csv = $.get('members.csv').then( d=>d);
-    contacts = $.get('contacts.json');
+    //  Fetch locally
+    members = $.get('members.json').then( d=>d);
+    contacts = $.get('contacts.json').then( d=>d);
   }
   else {
-    var token = verifyToken();
-    csv = token.then(getCsvThroughChannel);
-    contacts = token.then(getAllContacts);
+    //  Fetch from server
+    members = $.when().then( getMembersByChannel);
+    contacts = verifyToken().then( getContactsByXhr);
     if( fragment.state && fragment.state.debug) {
-      csv.done( d=>{makeDownloadLink(d, 'members.csv')} );
-      contacts.done( (o,s,xhr)=>{makeDownloadLink(xhr.responseText, 'contacts.json')} );
+      //  Make download links
+      members.done( d=>{makeDownloadLink(d, 'members.json')} );
+      contacts.done( d=>{makeDownloadLink(d, 'contacts.json')} );
     }
   }
-  csv.done( parseCsv);
+  members.done( console.log);
   contacts.done( console.log);
 
 });
 
-function parseCsv( csv) {
-  var reHead = /.+/g;
-  var reField = /"((?:""|[^"])*)"/g;
-
-  //  Get headers
-  var template = {};
-  var headers = reHead.exec(csv)[0]
-    .toLowerCase()
-    .match( reField)
-    .map( unquote)
-    .map( field => { 
-        var name = field.replace( 
-            /[^a-z0-9]+(.?)/g, 
-            (all,letter)=>letter.toUpperCase());
-        template[name] = field.match('/') && [] || undefined;
-        return name;
-      });
-  console.log( template);
-  template = JSON.stringify( template);
-
-  //  Get all field values (onedimensionally)
-  var allFields = csv
-    .substr( reHead.lastIndex)
-    .match( reField)
-    .map( unquote);
-  console.log( allFields);
-
-  //  Create member- and function dictionaries
-  var n=-1, members={}, current, functions={};
-  for( var f of allFields) {
-    n++; n %= headers.length;
-    if( !f) continue;
-    var header = headers[n];
-    if( n==0)
-      current = JSON.parse( template);
-    if( header == 'memberNumber')
-      members[f] = current;
-    if( header == 'activeFunctionIdsId') {
-      f = f.replace( /func-(\d+)-(\d+)-(\d+(?:-\d+)?)-(\d+)/, '$1-$3');
-      if( !functions[f]) functions[f] = [];
-      functions[f].push( current);
-    }
-    if( current[header] && current[header].push)
-      current[header].push(f);
-    else 
-      current[header] = f;
-  }
-  console.log(members,functions);
-
-  function unquote( f) {
-    return f
-      .substr( 1, f.length-2) //  Drop surrounding quotes
-      .replace( /""/g, '"');  //  Unescape quotes
-  }
-
-}
 
 
-function makeDownloadLink( data, filename, type='text/plain') {
-  var blob = new Blob( [data], {'type':type});
+function makeDownloadLink( data, filename, type='text/plain') 
+{
+  var blob = new Blob( [JSON.stringify(data)], {'type':type});
   var url  = URL.createObjectURL( blob);
   $('<a>')
     .attr('href',url)
     .attr('download', filename)
     .css('display','block')
+    .css('margin','1ex')
     .append('Download ' + filename)
     .prependTo(document.body);
 }
-function getCsvThroughChannel() {
+
+function getMembersByChannel() 
+{
   //  Establish channel
   var channel = new MessageChannel();
   var dfr = $.Deferred();
   channel.port1.onmessage = function(ev){
-    dfr.resolve(ev.data);
+    dfr.resolve( JSON.parse(ev.data));
+    channel.port1.close();
   };
   window.opener.postMessage(
     fragment.state.secret, '*', 
@@ -101,12 +52,9 @@ function getCsvThroughChannel() {
   return dfr;
 }
 
-function getAllContacts() {
-  //  Debug
-  if( fragment.debug == 'local')
-    return $.get('contacts.json');
-  else
-    return ajaxM8({'max-results':9000});
+function  getContactsByXhr() 
+{
+  return ajaxM8({'max-results':9000}).then( d=>d);
 }
 
 
@@ -116,7 +64,8 @@ function getAllContacts() {
 //  Returns true on success
 //
 var fragment = {};
-function parseFragment() {
+function parseFragment() 
+{
   var m, re=/(\w+)=([^&]*)/g;
   while( m = re.exec(window.location.hash))
     fragment[m[1]] = decodeURIComponent(m[2]);
@@ -129,7 +78,8 @@ function parseFragment() {
 //  
 var client_id = '637601984471-e5tn9qk8mds51b069vgd7tap5fl801re.apps.googleusercontent.com';
 var verifyUrl = 'https://www.googleapis.com/oauth2/v3/tokeninfo';
-function verifyToken() {
+function verifyToken() 
+{
   if( fragment.debug == 'local') 
     return $.Deferred().resolve();
   else if (!fragment.access_token) 
@@ -153,8 +103,13 @@ function verifyToken() {
 //    type:       json or xml
 //  Returns promise
 //
+//  Examples:
+//ajaxM8( {}, 'get', 'groups' );
+//ajaxM8( {q:'jens', group:'junior'}, 'get', 'contacts' );
+//
 function ajaxM8(data, method='get', feed='contacts',
-  projection='full', type='json') {
+  projection='full', type='json') 
+{
   if( type == 'xml' ){
     type = 'application/atom+xml';
     if( data.constructor == XMLDocument)
@@ -187,25 +142,6 @@ function ajaxM8(data, method='get', feed='contacts',
 /////////////////////////////////////////
 //
 //
-//
-//
-var rel = {
-  'home': 'http://schemas.google.com/g/2005#home',
-  'work': 'http://schemas.google.com/g/2005#work',
-  'other': 'http://schemas.google.com/g/2005#other',
-  'mobile': 'http://schemas.google.com/g/2005#mobile',
-};
-
-var rawPhone = {
-  'home': 'kontaktPrivatTlf',
-  'mobile': 'kontaktMobilTlf',
-  'work': 'kontaktArbejdeTlf',
-};
-
-var rawEmail = {
-  'home': 'kontaktPrimrEmail',
-  'other': 'kontaktEkstraEmailAdresser',
-};
 
 
 //
@@ -218,7 +154,8 @@ var rawEmail = {
 //
 //  Returns plain object as toplevel container with 'feed' property
 //
-function batchify( input, operation, template=null){
+function batchify( input, operation, template=null)
+{
   var i, o, n = 0, output=[];
   for (i of input) {
     e = template ? clone(template,[i]) : i;
@@ -236,7 +173,8 @@ function batchify( input, operation, template=null){
 //    Number: replace with sub[input]
 //    else:   reuse input
 //
-function clone( input, sub=[]) {
+function clone( input, sub=[]) 
+{
   if( input.constructor === Array)
     return input.map( e=>clone(e,sub) );
   else if( input.constructor === Object) {
@@ -256,7 +194,8 @@ function clone( input, sub=[]) {
 //    serialize:      Return string via XMLSerializer
 //    parentElement:  null; only for internal recursion
 //
-function toXml(obj, serialize=false, parentElement) {
+function toXml(obj, serialize=false, parentElement) 
+{
   if (parentElement) {  // Recursion
     var doc = parentElement.ownerDocument;
     for (var key in obj) {
@@ -289,30 +228,22 @@ function toXml(obj, serialize=false, parentElement) {
 }
 
 
-function createGroup(groupName) {
+function createGroup(groupName) 
+{
   if(!groupName)  groupName = 'G'+Date.now();
   console.log('createGroup', groupName);
   return  ajaxM8( {'entry':{'title':{'$t':groupName}}},
     'post', 'groups', 'full', 'json' );
 }
 
-function createContacts( data) {
+function createContacts( data) 
+{
   console.log('createContacts', data);
   data = batchify( data, 'insert');
   return ajaxM8( data, 'post', 'contacts', 'full/batch', 'xml');
 }
 
 
-function getGroups(data={}) {
-  return ajaxM8(data, 'get', 'groups' );
-}
-
-function getContacts(q,group) {
-  var data = {};
-  if(q) data.q = q;
-  if(group) data.group = group;
-  return ajaxM8( data, 'get', 'contacts' );
-}
 
 
 
@@ -326,128 +257,3 @@ var rel = {
   'mobile': 'http://schemas.google.com/g/2005#mobile',
 };
 
-var rawPhone = {
-  'home': 'kontaktPrivatTlf',
-  'mobile': 'kontaktMobilTlf',
-  'work': 'kontaktArbejdeTlf',
-};
-
-var rawEmail = {
-  'home': 'kontaktPrimrEmail',
-  'other': 'kontaktEkstraEmailAdresser',
-};
-
-//  Create group objects
-function handleCsv(csv) {
-  console.log('handleCsv');
-  var record, records=[], primary=[], secondary=[];
-  parseCsv( csv, (record)=>{
-    records.push(record);
-    var gcEntry = record.gcEntry = {
-      'gd$name': {'gd$fullName':{'$t':record.kontaktNavn}},
-      'gd$phoneNumber': [],
-      'gd$email': [],
-      'gd$structuredPostalAddress': [],
-      'gContact$groupMembershipInfo': [],
-    };
-    //  Add address
-    gcEntry.gd$structuredPostalAddress.push({
-      'rel': rel.home,
-      'gd$formattedAddress':{'$t':
-        record.kontaktAdresse+'\n'+record.kontaktPostnrby+'\n'+record.kontaktLandenavn }
-    });
-    for (var r in rel){
-      //  Add phone
-      var value = record[rawPhone[r]];
-      if( value)  gcEntry.gd$phoneNumber.push({
-        'rel':rel[r],
-        '$t':value,
-        'primary': r=='mobile'
-      });
-      //  Add email
-      value = record[rawEmail[r]];
-      if( value) gcEntry.gd$email.push({
-        'rel':rel[r],
-        'address':value,
-        'primary': r=='home'
-      });
-    }
-    //  Identify primary records
-    if( record.medlemsnavn == record.kontaktNavn) {
-      primary.push(record);
-      record.isPrimary = true;
-      //  Add primary-only fields
-      if(record.fdselsdato)
-        gcEntry.gContact$birthday = {'when': record.fdselsdato.split('-').reverse().join('-')};
-      if(record.indmeldelsesdato)
-        gcEntry.gContact$event = [{
-          'gd$when': {'startTime':record.indmeldelsesdato.split('-').reverse().join('-')},
-          'label': 'IndmeldtDDS'}];
-      if(record.medlemsnr)
-        gcEntry.gContact$externalId = [{
-          'value': record.medlemsnr,
-          'label':'Medlemsnr DDS'}];
-      if(record.kn)
-        gcEntry.gContact$gender = {'value': record.kn=='M'?'male':'female'};
-    }
-  });
-
-  //  Identify secondary records
-  for(record of records) if( !record.isPrimary )
-    secondary.push(record);
-  createAndFillGroup( primaryName, primary);
-  createAndFillGroup( secondaryName, secondary);
-}
-
-function parseCsv1( csv, recordCallback) {
-  console.log('parseCsv');
-  var reField = /([\r\n]*)"(([^"]|"")*)",/g;
-  var m, line;
-  var lines = [line=[]];
-
-  //  Break the CSV into lines and fields
-  while( m = reField.exec(csv)) {
-    if(m[1]) lines.push(line=[]);
-    line.push(m[2]
-      .replace(/""/g,'"')           //  Unescape ""
-      .replace(/[ \t]+/g,' ')       //  Collapse space
-      .replace(/^\s+|\s+$/g,''));   //  Trim
-  }
-
-  //  CameCase field names
-  var reBadChar = /[^a-z0-9 ]/g;
-  var reInitial = / +(.)/g;
-  var headers = lines.shift().map( (t)=>{
-    return t.toLowerCase()
-    .replace( reBadChar, '')
-    .replace( reInitial, (m)=>{return m[1].toUpperCase();});
-  });
-
-  //  Make record objects
-  for(line of lines) {
-    //  Name fields
-    var header, record = {};
-    for(header of headers)
-    record[header] = line.shift();
-    recordCallback(record);
-  }
-}
-
-function createAndFillGroup( groupName, contacts) {
-  console.log('createAndFillGroup', arguments);
-  createGroup(groupName).then((data,status,xhr)=>{
-    var groupid = data.entry.id.$t;
-    var updatedContacts=[], newContacts=[];
-    for (record of contacts) {
-      record.gcEntry.gContact$groupMembershipInfo.push({'href':groupid });
-      // if(record.gcEntry.id)
-      //   updatedContacts.push(record.gcEntry)
-      // else
-      newContacts.push(record.gcEntry)
-    }
-    // if(updatedContacts.length)
-    //   updateContact(updatedContacts);
-    // if(newContacts.length)
-    createContacts(newContacts);
-  });
-}
